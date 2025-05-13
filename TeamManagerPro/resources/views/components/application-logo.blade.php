a2d9c38962a0357b345a70076c0a5a4eb192f061
Marc <mcanadassuau@cifpfbmoll.eu>
2025-05-13 17:50:23 +0200

    Añadido nuevo logo sin letras

--- TeamManagerPro/resources/views/components/application-logo.blade.php
+++ TeamManagerPro/resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('imagenes/TeamManagerPro_Logo.png') }}" alt="TeamManagerPro Logo" class="w-24 h-auto mx-auto rounded-xl shadow-md">
+    <img src="{{ asset('imagenes/Logo.png') }}" alt="TeamManagerPro Logo" class="w-24 h-auto mx-auto rounded-xl shadow-md">
 </a>
